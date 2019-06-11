40a7b7b632555359701803558aef5ed3d0a9dafa
Subhojyoti Khastagir <Subhojyoti.Khastagir@hyland.com>
2019-06-11 15:22:08 +0530

    Basic functionality
    Created a basic layout for displaying time with double font size than
    normal. Time is displayed in the local standard time zone with the help
    of setInterval function. No rocket science!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'time';
+  private time: string;
+
+  constructor() {
+    this.time = new Date().toTimeString();
+
+    setInterval(() => {
+      this.time = new Date().toTimeString();
+    }, 500);
+  }
 }
